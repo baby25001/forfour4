36e6fcdd8c2e59a3eb79a57131d4d7b02f9f46c5
Yuushi!! <yoshiyushi77@gmail.com>
2026-01-29 22:31:24 +0700

    feat: bgm player finished

--- scenes/bgm_player.gd
+++ scenes/bgm_player.gd
@@ -4,6 +4,7 @@ extends Node2D
 var next_variation: int = 0
 var current_variation: int = 0
 var loop: int = 0
+var in_transition: bool = false
 
 func _ready() -> void:
 	var BPS = BPM/60
@@ -16,15 +17,33 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	loop += 1
-	if next_variation == 0:
-		play_loop()
+	if in_transition:
+		play_transition(current_variation, loop)
+		return
+	
+	if current_variation == next_variation or is_even(loop):
+		play_loop(current_variation)
+	if next_variation > current_variation and not in_transition and not is_even(loop):
+		current_variation += 1
+		print("TRANSITION!!")
+		in_transition = true
+		loop = 1
+		play_transition(current_variation, loop)
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		next_variation += 1
+		if next_variation > 2:
+			next_variation = 2
+	$Label.text = str(next_variation)
+	$Label2.text = str(current_variation)
 
 func is_even(number):
 	if number % 2 == 0:
 		return true
 	return false
 
-func play_loop():
+func play_loop(number):
 	if not is_even(loop):
 		print("play pad 1")
 		$Pad1.play()
@@ -32,3 +51,37 @@ func play_loop():
 		print("play pad 2")
 		$Pad2.play()
 	
+	if number >= 1:
+		$Perc.play()
+		$Drums.play()
+	
+	if number >= 2:
+		$Bass.play()
+
+func play_transition(variant, measure):
+	if variant == 1:
+		if measure == 1:
+			$Bass.play()
+			$Perc.play()
+		elif measure == 2:
+			$Bass.play()
+			$Perc.play()
+			$Drums.play()
+		elif measure == 3:
+			play_loop(variant)
+		elif measure == 4:
+			current_variation += 1
+			in_transition = false
+			play_loop(variant)
+	if variant == 2:
+		if measure == 1:
+			$Bass.play()
+		elif measure == 2:
+			$Bass.play()
+			$Pad2.play()
+		elif measure == 3:
+			play_loop(variant)
+		elif measure == 4:
+			current_variation += 1
+			in_transition = false
+			play_loop(variant)
